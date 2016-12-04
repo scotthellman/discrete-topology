794332e63c7c24584873dc35e0ca12cce9dc77d4
scotthellman <scott@scutellaris.com>
2016-12-04 15:11:16 -0700

    added ability to find cofaces

--- complex.py
+++ complex.py
@@ -1,35 +1,71 @@
 # -*- coding: utf-8 -*-
+from collections import defaultdict
+
 
 class SimplexTree:
     
     def __init__(self):
-        self.root = SimplexTreeNode("root", None)
+        self.root = SimplexTreeNode("root", None,-1)
+        self.sibling_tree = defaultdict(lambda : defaultdict(list))
+        self.depth = 0
         
     def add_simplex(self, nodes):
         nodes = sorted(nodes)
         for i in range(len(nodes)):
             subnodes = nodes[i:]
             parent = self.root
-            for node in subnodes:
+            for j,node in enumerate(subnodes):
+                #i+j is depth
                 current = parent.children
                 if node not in current:
-                    current[node] = SimplexTreeNode(node, parent)
-                parent = current[node]
-            
+                    current[node] = SimplexTreeNode(node, parent, i+j)
+                self.sibling_tree[i+j][node].append(current[node])
+                parent = current[node]   
+                if i+j > self.depth:
+                    self.depth = i+j
+                
+    def find_node(self, simplex):
+        current = self.root
+        for node in simplex:
+            current = current.children[node]
+        return current
+    
+    def find_cofaces(self, simplex):
+        #find all simplices that have the same final vertex
+        #nb this doesn't actually return ALL cofaces - things of larger dim
+        #may just be implied
+        final_node = self.find_node(simplex)
+        cofaces = []
+        for dim in range(final_node.dimension, self.depth + 1):
+            siblings = self.sibling_tree[dim][final_node.name]
+            for sibling in siblings:
+                if sibling == final_node:
+                    continue
+                path = sibling.path_to_root()
+                names = set([p.name for p in path])
+                if all([s in names for s in simplex]):
+                    cofaces.append(sibling)
+        return cofaces
 
 
 class SimplexTreeNode:
     
-    def __init__(self, name, parent):
+    def __init__(self, name, parent, dim):
         self.name = name
-        self.siblings = []
         self.parent = parent
+        self.dimension = dim
         self.children = {}
         
     def __str__(self):
         return "{}: {}".format(self.name, [str(c) for c in self.children.values()])
         
-
+    def path_to_root(self):
+        path = []
+        current = self
+        while current is not None:
+            path.append(current)
+            current = current.parent
+        return path
 
 if __name__ == "__main__":
     simplices = [[1,2,3,4],[3,4,5]]
@@ -38,3 +74,6 @@ if __name__ == "__main__":
     tree.add_simplex(simplices[0])
     tree.add_simplex(simplices[1])
     print(tree.root)
+    print(tree.find_node([1,2,3]))
+    print("-")
+    print(tree.find_cofaces([3,4]))
